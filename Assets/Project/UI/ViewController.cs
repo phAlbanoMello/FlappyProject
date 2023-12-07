3f1f88bb79e96133f1549bc0c5033862969ba17e
pedro.albano <pedro.albano@DESKTOP-HC6RJSF>
2023-12-07 19:34:20 -0300

    Reorganized file structure and created some UI classes for later implementation.

--- Assets/Project/UI/ViewController.cs
+++ Assets/Project/UI/ViewController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IEntity : MonoBehaviour
+public class ViewController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
